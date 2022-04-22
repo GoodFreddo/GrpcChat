6cbf04fe6424aa9c443d64b7a6d06b144aee8044
Lachlan <goodfreddo@yahoo.co.uk>
2022-04-22 16:53:10 +0100

    Add project files.

---
+++ GrpcChatServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using System;
+using GrpcChatContract;
+
+namespace GrpcChatServer
+{
+    class GrpcChatImplimentation : GrpcChat.GrpcChatBase
+    {
+
+    }
+    class Program
+    {
+        const int Port = 12345;
+        static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { GrpcChat.BindService(new GrpcChatImplimentation())},
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Chat server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
